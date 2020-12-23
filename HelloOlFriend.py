9825b3a05cbcdb0c407b49d82611be84f4a176d1
Justisqq <73496237+Justisqq@users.noreply.github.com>
2020-12-23 14:59:49 +0000

    Adedd y axis and boundaries

--- HelloOlFriend.py
+++ HelloOlFriend.py
@@ -5,9 +5,8 @@ import pygame
 import sys
 import os
 
-display_height = 500
-display_width = 500
-
+display_height = 480
+display_width = 640
 grid_size = 50
 grid_height = display_height / grid_size
 grid_width = display_width / grid_size
@@ -17,7 +16,9 @@ gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('DIABLO')
 clock = pygame.time.Clock()
 
-Img1 = pygame.image.load(os.path.join("images","Diablo sticker.png"))
+Img1 = pygame.image.load(os.path.join("images","Humanimage.png"))
+gameIcon = pygame.image.load(os.path.join("images","Diablo sticker2.png"))
+pygame.display.set_icon(gameIcon)
 
 silver = (192, 192, 192)                          #RGB based so (more light bigger number,     ,     )
 gray = (128, 128, 128)
@@ -33,9 +34,9 @@ lime = (0, 255, 0)
 black = (0, 0, 0)
 beige = (245, 245, 220)
 
-char_width = 84
+char_width = 80
 char_height = 80
-
+##
 class Character:
     def __init__(self):
         self.name = "Bob"
@@ -43,13 +44,13 @@ class Character:
 
     def move(self):
         pass
-
+##
 class Npc:
     def __init__(self):
         self.name = ""
         self.position = display_width * 0.8, display_height * 0.7
 
-
+##
 def draw_grid(surface):
     for y in range(0, int(grid_height)):
         for x in range(0, int(grid_width)):
@@ -63,11 +64,15 @@ def draw_grid(surface):
 def charImage(x,y):
         gameDisplay.blit(Img1, (x  ,y ))
 
+#def processEvents():
+
+
 def run_game():
     dead = False
     x = display_width * 0.8
     y = display_height * 0.8
     x_change = 0
+    y_change = 0
 
     while not dead:
 
@@ -75,21 +80,37 @@ def run_game():
             if event.type == pygame.QUIT:
                 dead = True
 
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -5
                 elif event.key == pygame.K_RIGHT:
                     x_change = 5
+                if event.key == pygame.K_UP:
+                    y_change = -5
+                elif event.key == pygame.K_DOWN:
+                    y_change = 5
 
-            if event.type == pygame.KEYUP:
+            elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+                elif event.key == pygame.K_UP or pygame.K_DOWN:
+                    y_change = 0
 
-        if x < display_width - char_width + 5 and x - 5 > 0:
+        if x - 5 > 0 and x < display_width - char_width + 5:
             x += x_change
-        else:
-            x -= x_change
-     
+        elif x - 5 <= 0:
+            x += 1
+        elif x >= display_width - char_width + 5:
+            x -= 1
+
+        if y - 5 > 0 and y < display_height - char_height + 5:
+            y += y_change
+        elif y - 5 <= 0:
+            y += 1
+        elif y >= display_height - char_height + 5:
+            y -= 1
+
+        
         gameDisplay.fill(gray)
         charImage(x, y)    
 
